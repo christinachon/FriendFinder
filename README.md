a34df03f6123e1a51aa870820c8d5a33ad779910
Christina <christinachon90@gmail.com>
2019-08-22 23:08:32 -0700

    added server files and code to take in data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FriendFinder
+A website that takes in data to find the closest match from another user.
+
+##Technology Used
+1. Node.js
+2. Express & Handlebars
+3. Bootstrap
---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsList = [
+	{
+	  name: "ross",
+	  photo: "https://cdn.evoke.ie/wp-content/uploads/2018/04/04155521/Ross-Friends-696x503.jpg",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "phoebe",
+	  photo: "https://amp.businessinsider.com/images/55df18389dd7cc0f008b64cc-750-563.jpg",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "joey",
+	  photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F07%2F03011013%2Ffriends.jpg&w=400&c=sc&poi=face&q=85",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+];
+
+module.exports = friendsList;
